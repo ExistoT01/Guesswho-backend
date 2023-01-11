d937eb974d1f1014e4a49db9d222ed5ce0ad7a65
ExistoT01 <1x6tot01@gmail.com>
2023-01-11 17:54:22 +0800

    first commit

---
+++ src/main/java/com/existot01/guesswho/GuessWhoApplication.java
@@ -0,0 +1,13 @@
+package com.existot01.guesswho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessWhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessWhoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/existot01/guesswho/GuessWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.existot01.guesswho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessWhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
